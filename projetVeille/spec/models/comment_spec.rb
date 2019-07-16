70b79eb29b54ca270ceb85a742c0dd11ef00ee94
muhammad-tounsi <muhammad.tounsi@hotmail.fr>
2019-07-16 15:18:40 +0200

    FEAT: Test unitaire

--- projetVeille/spec/models/comment_spec.rb
+++ projetVeille/spec/models/comment_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:article) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:message) }
+  it { should validate_presence_of(:article_id) }
 end
